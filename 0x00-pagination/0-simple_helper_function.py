ec826d09024524fabfa4f2eb17b3e9d12ef0c24a
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-19 16:01:47 +0100

    Tried task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,9 +7,10 @@ containing a start index and an end index
 corresponding to the range of indexes to return 
 in a list for those particular pagination parameters.
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     initialized index_range func
     """
