0b8d7762b2135554a35bec53d2d58ed2edd782f9
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-19 20:22:22 +0100

    Tried task 2

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -73,3 +73,12 @@ class Server:
         }
         return hyper_data
     
+if __name__ == "__main__":
+    server = Server()
+    print(server.get_hyper(1, 2))
+    print("---")
+    print(server.get_hyper(2, 2))
+    print("---")
+    print(server.get_hyper(100, 3))
+    print("---")
+    print(server.get_hyper(3000, 100))
