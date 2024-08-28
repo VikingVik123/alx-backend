de194d7821281889d0b764e6033450e712ecf6b0
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-28 04:51:27 +0100

    added task 2

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+setup a basic flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    instiantiated config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello_world():
+    """
+    method 2 return hello world
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
