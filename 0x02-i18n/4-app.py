787db15ed91790ea59a55202a0ccad382a7a9942
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-29 13:14:25 +0100

    Did task 3&4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,14 +28,12 @@ def hello_world():
     """
     return render_template("1-index.html")
 
-
 @babel.localeselector
 def get_locale():
     """
     Determine the best match for supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 if __name__ == '__main__':
     app.run(debug=True)
